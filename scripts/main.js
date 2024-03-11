51419a7b32212a3ac79719f2fb7f86b259b73583
ankitpal1855 <154039596+ankitpal1855@users.noreply.github.com>
2024-03-11 15:45:44 +0530

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+// function myfunc(){
+//     if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+//         document.getElementById("navbar").classList.remove("transparent");
+//         document.getElementById("navbar").classList.add("colored");
+//     } else {
+//         document.getElementById("navbar").classList.remove("colored");
+//         document.getElementById("navbar").classList.add("transparent");
+//     }
+// }
+
+// Window.onscroll= myfunc();
